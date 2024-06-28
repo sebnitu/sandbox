f0200d3b9fc556f540da2b63fe92b895eef393d2
sebnitu <nitu.seb@gmail.com>
2024-06-28 10:49:43 -0700

    Remove font awesome in favor of arrow utility and svg x

--- src/multi-select.js
+++ src/multi-select.js
@@ -62,7 +62,7 @@ function updateSelectedList(el, selected, defaultValue = 'Nothing selected yet..
       button.setAttribute('data-multi-select-remove', item.toLowerCase());
       button.classList.add('button');
       button.innerHTML = '<span>' + item + '</span> ' +
-        '<span class="fas fa-times" aria-hidden="true"></span>' +
+        '<svg class="icon icon_size_sm" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>' +
         '<span class="sr-only">Unselect</span>';
       li.appendChild(button);
       list.appendChild(li);
