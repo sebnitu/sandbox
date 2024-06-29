d1fa8914cc20e5adf4c9f58daaae62a70e6264fe
sebnitu <nitu.seb@gmail.com>
2024-06-28 17:58:17 -0700

    Create swapNodes module

--- src/draggable.js
+++ src/draggable.js
@@ -1,3 +1,5 @@
+import { swapNodes } from "./swapNodes";
+
 const list = document.querySelector(".sortable");
 const items = document.querySelectorAll(".sortable__item");
 
@@ -53,7 +55,7 @@ items.forEach((item) => {
     item.classList.remove("is-dragover");
 
     // Swap the two elements in the dom.
-    swap(dragging, item);
+    swapNodes(dragging, item);
   });
 
   /**
@@ -85,23 +87,3 @@ items.forEach((item) => {
   });
 
 });
-
-/**
- * Swaps the position of two elements in the DOM.
- * @param {HTMLElement} node1 
- * @param {HTMLElement} node2 
- */
-function swap(node1, node2) {
-  // Create marker element and insert it where node1 is.
-  const temp = document.createElement("div");
-  node1.parentNode.insertBefore(temp, node1);
-
-  // Move node1 to right before node2.
-  node2.parentNode.insertBefore(node1, node2);
-
-  // Move node2 to right before where node1 used to be.
-  temp.parentNode.insertBefore(node2, temp);
-
-  // Remove temporary marker node.
-  temp.parentNode.removeChild(temp);
-}
