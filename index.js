dfa7ca5e2c26053b10e93cd2dd9266dafbc0334b
sebnitu <nitu.seb@gmail.com>
2025-05-13 16:18:56 -0700

    Initial calendar markup and styles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-import * as vrembem from "https://unpkg.com/vrembem@next/dist/index.js";
+import "./src/app.scss";
+import * as vrembem from "https://unpkg.com/vrembem@next/dev/index.js";
 
 console.log("Vrembem", vrembem);
