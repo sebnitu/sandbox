1744f064ce177ae8441d9c972713091389f93655
sebnitu <nitu.seb@gmail.com>
2026-04-12 11:26:53 -0700

    Add radio and switch components

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 /// <reference types="vite/client" />
 
+// Lines
+// ---
+// Individual: 132
+// Shared Class: 
+
+class VrembemChoice extends HTMLElement {
+
+}
+
 class CheckboxComponent extends HTMLElement {
   #initialized = false;
   input!: HTMLInputElement;
@@ -45,4 +54,90 @@ class CheckboxComponent extends HTMLElement {
   }
 }
 
+class RadioComponent extends HTMLElement {
+  #initialized = false;
+  input!: HTMLInputElement;
+
+  static modifiers = {
+    size: "radio_size_"
+  }
+
+  connectedCallback() {
+    // Only run this if it hasn't been initialized
+    if (this.#initialized) return;
+    this.#initialized = true;
+
+    // Add component class to the host element
+    this.classList.add("radio");
+
+    // Get or create the input element
+    this.input = this.querySelector("input") || document.createElement("input");
+    this.input.type = "radio";
+    this.input.classList.add("radio__native");
+
+    // Build wrapper markup and add the input
+    this.innerHTML = `
+      <span class="radio__background">
+        <span class="radio__circle">
+          <span class="radio__dot"></span>
+        </span>
+      </span>
+    `;
+
+    // Prepend the input
+    this.prepend(this.input);
+
+    // Set modifiers
+    for (const [modifier, prefix] of Object.entries(CheckboxComponent.modifiers)) {
+      if (this.hasAttribute(modifier)) {
+        this.classList.add(`${prefix}${this.getAttribute(modifier)}`);
+      }
+    }
+  }
+}
+
+class SwitchComponent extends HTMLElement {
+  #initialized = false;
+  input!: HTMLInputElement;
+
+  static modifiers = {
+    size: "switch_size_"
+  }
+
+  connectedCallback() {
+    // Only run this if it hasn't been initialized
+    if (this.#initialized) return;
+    this.#initialized = true;
+
+    // Add component class to the host element
+    this.classList.add("switch");
+
+    // Get or create the input element
+    this.input = this.querySelector("input") || document.createElement("input");
+    this.input.type = "checkbox";
+    this.input.classList.add("switch__native");
+
+    // Build wrapper markup and add the input
+    this.innerHTML = `
+      <span class="switch__background">
+        <span class="switch__track">
+          <span class="switch__thumb"></span>
+        </span>
+      </span>
+    `;
+
+    // Prepend the input
+    this.prepend(this.input);
+
+    // Set modifiers
+    for (const [modifier, prefix] of Object.entries(CheckboxComponent.modifiers)) {
+      if (this.hasAttribute(modifier)) {
+        this.classList.add(`${prefix}${this.getAttribute(modifier)}`);
+      }
+    }
+  }
+}
+
 customElements.define("vb-checkbox", CheckboxComponent);
+customElements.define("vb-radio", RadioComponent);
+customElements.define("vb-switch", SwitchComponent);
