04c2052772714402a88e95060549d90de18d8121
sebnitu <nitu.seb@gmail.com>
2026-04-11 22:44:19 -0700

    Simplify the implementation by only supporting the slotted input method

--- index.ts
+++ index.ts
@@ -1,56 +1,25 @@
 /// <reference types="vite/client" />
 
 class CheckboxComponent extends HTMLElement {
-  #initialized: boolean = false;
+  #initialized = false;
   input!: HTMLInputElement;
   root!: HTMLElement;
 
-  // Declare which host attributes to watch
-  static observedAttributes = ["name", "value", "checked", "disabled", "required", "indeterminate", "aria-checked", "size"];
-
-  // Create modifier map
-  static modifiers: Record<string, string> = {
-    size: "checkbox_size"
-  };
-  
-  private applyModifiers(modifier?: string) {
-    if (!modifier) {
-      // Loop through the modifiers map and run recursively
-      for (const modifier of Object.keys(CheckboxComponent.modifiers)) {
-        this.applyModifiers(modifier);
-      }
-      return;
-    }
-
-    // Get the prefix and return if it doesn't exist.
-    const prefix = CheckboxComponent.modifiers[modifier];
-    if (!prefix) return;
-
-    // Remove the existing modifier if it exists
-    this.root.classList.forEach((item) => {
-      if (item.startsWith(`${prefix}_`)) this.root.classList.remove(item);
-    });
-
-    // Add the modifier provided by the host attribute
-    const value = this.getAttribute(modifier);
-    if (value) {
-      this.root.classList.add(`${prefix}_${value}`);
-    }
+  static modifiers = {
+    size: "checkbox_size_"
   }
 
   connectedCallback() {
-    // Check if this element has been initialized
+    // Only run this if it hasn't been initialized
     if (this.#initialized) return;
     this.#initialized = true;
 
-    // Get the input element
-    this.input = this.querySelector("input") || document.createElement("input");   
-    
-    // Ensure that the type attribute and native class are set
-    this.input.setAttribute("type", "checkbox");
+    // Get or create the input element
+    this.input = this.querySelector("input") || document.createElement("input");
+    this.input.type = "checkbox";
     this.input.classList.add("checkbox__native");
 
-    // Insert the checkbox markup
+    // Build wrapper markup and add the input
     this.innerHTML = `
       <span class="checkbox">
         <span class="checkbox__background">
@@ -61,68 +30,18 @@ class CheckboxComponent extends HTMLElement {
       </span>
     `;
 
-    // Set the root component element
+    // Prepend the input
     this.root = this.querySelector(".checkbox")!;
-
-    // Prepend the input in the markup. We know ".checkbox" will be available 
-    // because we inject it just above.
     this.root.prepend(this.input);
 
-    // Sync host attributes to the input on init
-    for (const attr of CheckboxComponent.observedAttributes) {
-      if (this.hasAttribute(attr)) {
-        // Skip modifier attributes
-        if (attr in CheckboxComponent.modifiers) continue;
-        // We need to set indeterminate differently than other attributes
-        if (attr === "indeterminate") {
-          this.input.indeterminate = true;
-          continue;
-        }
-        // Apply the rest of the attributes directly to input
-        this.input.setAttribute(attr, this.getAttribute(attr) || "");
+    // Maybe set the indeterminate state
+    this.input.indeterminate = this.input.matches('[aria-checked="mixed"]');
+
+    // Set modifiers
+    for (const [modifier, prefix] of Object.entries(CheckboxComponent.modifiers)) {
+      if (this.hasAttribute(modifier)) {
+        this.root.classList.add(`${prefix}${this.getAttribute(modifier)}`);
       }
-    }
-
-    // Get checkboxes with "mixed" status
-    if (this.input.matches("[aria-checked='mixed']")) {
-      this.input.indeterminate = true;
-    }
-
-    // Apply provided modifiers to the root checkbox element
-    const modifiers = this.getAttribute("modifiers");
-    if (modifiers) {
-      this.root.classList.add(...modifiers.split(/\s+/));
-    }
-
-    // Apply modifier attribute maps to the root checkbox element
-    this.applyModifiers();
-  }
-
-  // React to host attribute changes at any time
-  attributeChangedCallback(name: string, oldValue: string | null, newValue: string | null) {
-    // Guard in case input hasn't been set yet
-    if (!this.input) return;
-
-    // Check if the checkbox is rendered
-    if (!this.root) return;
-
-    // Apply the modifiers if the attribute was passed
-    if (name in CheckboxComponent.modifiers) {
-      this.applyModifiers(name);
-      return;
-    }
-
-    // We need to set indeterminate differently than other attributes
-    if (name === "indeterminate") {
-      this.input.indeterminate = newValue !== null;
-      return;
-    }
-
-    // Update or remove the attribute value
-    if (newValue === null) {
-      this.input.removeAttribute(name);
-    } else {
-      this.input.setAttribute(name, newValue);
     }
   }
 }
