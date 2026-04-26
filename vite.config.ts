f604605b2be755638253ddcc5de3c9cd5543854d
sebnitu <nitu.seb@gmail.com>
2026-04-26 08:43:50 -0700

    Update demo markup and use more vite env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "vite";
+import data from "./package.json";
 
 export default defineConfig({
   define: {
+    "import.meta.env.VITE_REPO": JSON.stringify("https://github.com/sebnitu/sandbox"),
     "import.meta.env.VITE_BRANCH": JSON.stringify("main"),
+    "import.meta.env.VITE_NAME": JSON.stringify(data.name),
+    "import.meta.env.VITE_DESC": JSON.stringify(data.description),
+    "import.meta.env.VITE_FAVICON": JSON.stringify("https://sebnitu.com/icons/favicon.png")
   },
 });
