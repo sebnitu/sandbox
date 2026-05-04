19ef22fdce1bf405c848434158daba1627f30570
sebnitu <nitu.seb@gmail.com>
2026-05-04 08:52:21 -0700

    Add drawer based layout

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
+// @ts-ignore
 import "./index.scss";
+import { DrawerCollection, mediaQuery } from "vrembem";
+
+const drawers = new DrawerCollection({
+  plugins: [mediaQuery()]
+});
+
+drawers.mount();
