1d256393635ac23e6505183ece83f12bf3fd271c
sebnitu <nitu.seb@gmail.com>
2026-08-15 20:22:21 -0700

    Update demo markup for most recent Vrembem

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import sortable from "./src/sortable";
 
 sortable("sortable-list", {
   onUpdate(item) {
-    // Example of desifing the onUpdate callback
+    // Example of using the onUpdate callback
     const list = Array.from(this.list.querySelectorAll(this.settings.items));
     const index = list.indexOf(item);
     console.log("Save sort...", item, index);
